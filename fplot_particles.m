a4b90e568db69b58bcae61d8acb788a143901212
Kiah <miciahjohnhenderson@gmail.com>
2022-11-30 10:52:10 -0500

    3d w/ adsorption

--- fplot_particles.m
+++ fplot_particles.m
@@ -2,17 +2,20 @@
 % fplot_particles.m
 % A function to plot the particles on an LxL lattice
 
-function fplot_particles(L,Np,xpos,ypos,Nfig)
+function fplot_particles(L, h, Np, xpos, ypos, zpos, Nfig)
 
-figure(Nfig); clf
-plot(xpos,ypos,'ro','MarkerSize',20,'MarkerFaceColor','r')
-xlabel('x'); 
-ylabel('y');
-axis equal
-axis([0.5 L+0.5 0.5 L+0.5])
-grid on
-axis square
-set(gca,'Ytick',1:L);
-set(gca,'Xtick',1:L);
-set(gca,'FontSize',14)
+    figure(Nfig); clf
+    plot3(xpos, ypos, zpos, 'ro', 'MarkerSize', 20, 'MarkerFaceColor', 'r')
+    xlabel('x');
+    ylabel('y');
+    zlabel('z');
+    axis equal
+    axis([0.5 L + 0.5 0.5 L + 0.5])
+    grid on
+    axis square
+    set(gca, 'Ytick', 1:L);
+    set(gca, 'Xtick', 1:L);
+    set(gca, 'Ztick', 1:h);
+    set(gca, 'FontSize', 14)
 
+end
